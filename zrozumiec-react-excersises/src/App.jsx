fa20168d10fb6aaa97e68a64c4cc7c90facd7ac6
Katarzyna Zaloba <kzalobafrontend@gmail.com>
2024-07-01 18:29:41 +0200

    Prepare app to the exercises

---
+++ zrozumiec-react-excersises/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+    return <>
+        <h1>
+            Gwiezdne wojny V
+        </h1>
+        <h2>
+            Rok produkcji: 1980
+        </h2>
+        <p>
+            Dobrzy walczą ze złymi. Trzeba wyłączyć pole siłowe.
+        </p>
+        <button>
+            Pokaż spoiler
+        </button>
+        <p>
+            Vader okazuje się być ojcem Luka.
+        </p>
+    </>;
+}
+
+export default App;
